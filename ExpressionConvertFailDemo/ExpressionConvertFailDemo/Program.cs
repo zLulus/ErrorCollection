72c8450cd2dedbd8ca4f4044360e471b4a019a85
zhaotianff <zhaotianff@163.com>
2021-04-17 14:02:41 +0800

    hello world

--- ExpressionConvertFailDemo/ExpressionConvertFailDemo/Program.cs
+++ ExpressionConvertFailDemo/ExpressionConvertFailDemo/Program.cs
@@ -7,6 +7,7 @@ namespace ExpressionConvertFailDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Talk is cheap,show me the code.");
         }
     }
 }
